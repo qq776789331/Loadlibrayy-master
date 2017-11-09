4eb979ffd8d6d8f81263ed9f70a01442cbc4c78a
qq776789331 <33519343+qq776789331@users.noreply.github.com>
2017-11-09 22:09:08 +0800

    Initial commit

---
+++ Forms/NewInjectionForm.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Loadlibrayy.Forms
+{
+    partial class NewInjectionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InjectButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // InjectButton
+            // 
+            this.InjectButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.InjectButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.InjectButton.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InjectButton.ForeColor = System.Drawing.Color.Black;
+            this.InjectButton.Location = new System.Drawing.Point(12, 79);
+            this.InjectButton.Name = "InjectButton";
+            this.InjectButton.Size = new System.Drawing.Size(225, 46);
+            this.InjectButton.TabIndex = 1;
+            this.InjectButton.TabStop = false;
+            this.InjectButton.Text = "注入游戏";
+            this.InjectButton.UseVisualStyleBackColor = true;
+            this.InjectButton.Click += new System.EventHandler(this.InjectButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "by: 腾训公司 www.52pojie.cn";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(228, 34);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "请先进入游戏(匹配成功后的倒计时等待大厅),然后点击注入游戏";
+            // 
+            // NewInjectionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(249, 140);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.InjectButton);
+            this.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "NewInjectionForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PUBG Chams";
+            this.Load += new System.EventHandler(this.NewInjectionForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button InjectButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Forms/NewInjectionForm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DriverExploits;
+using Loadlibrayy.Injection;
+using Loadlibrayy.Logger;
+
+namespace Loadlibrayy.Forms
+{
+    public partial class NewInjectionForm : Form
+    {
+
+        public NewInjectionForm()
+        {
+            InitializeComponent();
+        }
+
+        private void InjectButton_Click(object sender, EventArgs e)
+        {
+            Process[] pros = Process.GetProcessesByName("TslGame");
+            Console.WriteLine(pros.Length);
+            // SANITY CHECKS
+            if (pros.Length == 0)
+            {
+                Log.ShowError("请先运行游戏,并且匹配成功进入游戏等待倒计时大厅", "错误");
+                return;
+            }
+            // LOAD EXPLOITABLE DRIVER 
+            bool driverLoaded = false;
+
+            if (!(driverLoaded = ElevateHandle.Driver.Load()))
+            {
+                Log.ShowError("CPUZ141.sys failed to load", "lol fuck");
+                return;
+            }
+
+            ElevateHandle.UpdateDynamicData(); // UPDATE KERNEL OFFSETS
+            ElevateHandle.Attach();            // ATTACH TO CURRENT PROCESS
+            ElevateHandle.Elevate((ulong)pros[0].Handle, 0x1fffff);
+
+
+            InjectionOptions options = new InjectionOptions()
+            {
+                ElevateHandle = true,
+                EraseHeaders = true,
+                CreateLoaderReference = false,
+                LoaderImagePath = Environment.CurrentDirectory + "\\ts.dll"
+            };
+
+            ExecutionType executionType = ExecutionType.HijackThread;
+
+            IInjectionMethod injectionMethod = new ManualMapInjection(pros[0], executionType, options);
+            //IInjectionMethod injectionMethod = new LoadLibraryInjection(g_SelectedProcess, executionType, options);
+            injectionMethod.InjectImage(Environment.CurrentDirectory + "\\ts.dll");
+
+            if (driverLoaded)
+                ElevateHandle.Driver.Unload();
+        }
+
+        private void NewInjectionForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
